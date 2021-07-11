dab44b1e32c86a7e316d232c47a2b44388eb6517
Drecra <drecra@outlook.com>
2021-07-11 22:03:14 +0800

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,6 +16,7 @@ class UsersController extends Controller
     {
         return view('users.show',compact('user'));
     }
+
     public function store(Request $request)
     {
         $this->validate($request,[
@@ -23,6 +24,13 @@ class UsersController extends Controller
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        session()->flash('success', '恭喜您注册成功！您将在这里开启一段新旅途~');
+        return redirect()->route('users.show',[$user]);
     }
 }
