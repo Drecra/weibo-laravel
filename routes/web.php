81b6e3a53ce97682abc8500d2dd8195687a54acb
Drecra <drecra@outlook.com>
2021-07-14 22:30:16 +0800

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/','StaticPagesController@home')->name('home');
+Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
-Route::get('signup','UsersController@create')->name('signup');
-Route::resource('users','UsersController');
+Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
