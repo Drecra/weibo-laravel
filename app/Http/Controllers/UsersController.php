aaad13ea0ea4ec5586bf5ae98367c9fbc691c0d6
Drecra <drecra@outlook.com>
2021-07-17 00:21:46 +0800

    登录成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
@@ -14,12 +15,12 @@ class UsersController extends Controller
 
     public function show(User $user)
     {
-        return view('users.show',compact('user'));
+        return view('users.show', compact('user'));
     }
 
     public function store(Request $request)
     {
-        $this->validate($request,[
+        $this->validate($request, [
             'name' => 'required|unique:users|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
@@ -30,7 +31,9 @@ class UsersController extends Controller
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
+
+        Auth::login($user);
         session()->flash('success', '恭喜您注册成功！您将在这里开启一段新旅途~');
-        return redirect()->route('users.show',[$user]);
+        return redirect()->route('users.show', [$user]);
     }
 }
