55d42dd0d19a2c4532b643ed04a0f2d4cfd4598f
Drecra <drecra@outlook.com>
2021-07-27 23:10:20 +0800

    管理员可删除用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -60,15 +60,23 @@ class UsersController extends Controller
         return redirect()->route('users.show', $user);
     }
 
-    public function __construct()
-    {
-        $this->middleware('auth',['except'=> ['show','create','store','index']
-        ]);
-    }
-
     public function index()
     {
         $users = User::paginate(6);
         return view('users.index',compact('users'));
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy',$user);
+        $user->delete();
+        session()->flash('success','成功删除用户');
+        return back();
+    }
+
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=> ['show','create','store','index']
+        ]);
     }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Drecra';
         $user->email = 'drecra@outlook.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
