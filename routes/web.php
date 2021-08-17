f905f6e2ea88a05c8fc5dd7e9e2460390cd1ee1c
Drecra <drecra@outlook.com>
2021-08-17 22:33:55 +0800

    发布微博

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,5 @@ Route::post('password/email','PasswordController@sendResetLinkEmail')->name('pas
 
 Route::get('password/reset/{token}','PasswordController@showResetForm')->name('password.reset');    //显示更新密码的表单，包含 token
 Route::post('password/reset','PasswordController@reset')->name('password.update');                  //对提交过来的 token 和 email 数据进行配对，正确的话更新密码
+
+Route::resource('statuses','StatusesController',['only'=>['store','destroy']]);
